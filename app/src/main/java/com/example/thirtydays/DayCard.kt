e796753fc73fee44c01ab193d4b112080460a3fa
AdamTegelberg <adam.tegelberg@gmail.com>
2023-11-24 00:14:34 +0100

    Bilder har samma storlek, fixat bakgrunds färg på Card

--- app/src/main/java/com/example/thirtydays/DayCard.kt
+++ app/src/main/java/com/example/thirtydays/DayCard.kt
@@ -4,13 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,13 +27,17 @@ import com.example.thirtydays.ui.theme.ThirtyDaysTheme
 
 @Composable
 fun DayCard(
-    day: Day = DaysList.days[1],
+    day: Day = DaysList.days[6],
     modifier: Modifier = Modifier
 ) {
 
     Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
         modifier = Modifier
             .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
 
     ) {
         Column {
@@ -35,8 +45,15 @@ fun DayCard(
             Box(
                 modifier = Modifier
                     .padding(8.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .fillMaxWidth()
+                    .height(200.dp)
             ) {
-                Image(painter = painterResource(id = day.image), contentDescription = null)
+                Image(
+                    painter = painterResource(id = day.image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
             }
             Text(
                 text = stringResource(id = day.description),
@@ -55,10 +72,10 @@ fun DayCard(
 fun TopText(day: Day, modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(8.dp)
     ) {
-        Text(text = stringResource(id = day.title), fontSize = 12.sp)
-        Text(text = stringResource(id = day.task))
+        Text(text = stringResource(id = day.title))
+        Text(text = stringResource(id = day.task), fontSize = 12.sp)
     }
 
 }
@@ -68,6 +85,11 @@ fun TopText(day: Day, modifier: Modifier = Modifier) {
 @Composable
 fun DayCardPreview() {
     ThirtyDaysTheme {
-        DayCard()
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            DayCard()
+        }
+
     }
 }
